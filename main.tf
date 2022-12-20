d30d9864d7a803d480c1af74a0c83df3abd663cb
Peter F <furuby_89@hotmail.com>
2022-12-20 14:14:45 +0100

    Point backend to correct TFC org and workspace.

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "AGC-Terraform-Labs-PF"
 
     workspaces {
-      name = "REPLACE_ME"
+      name = "demo-github-actions"
     }
   }
 }

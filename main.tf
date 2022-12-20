0ba66e4788bf915c34298091950f6a9a2474dbaa
Peter F <furuby_89@hotmail.com>
2022-12-20 15:23:34 +0100

    fixed correct TFC org, was incorrect.

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "AGC-Terraform-Labs-PF"
+    organization = "ACG-Terraform-Labs-PF"
 
     workspaces {
       name = "demo-github-actions"
